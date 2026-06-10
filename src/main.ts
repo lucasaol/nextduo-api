da392aaad9c740d475062ed23e76d1c11d3eeed6
Lucas Andrade <eudevlucas@gmail.com>
2026-06-10 11:43:13 -0300

    chore: enable cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { ClassSerializerInterceptor } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
   app.useGlobalInterceptors(
     new ClassSerializerInterceptor(app.get(Reflector))
